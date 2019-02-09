b24556bc850413544a9d4a34a6f54d736f089508
Carl Argabright <carl.argabright@gmail.com>
2019-02-09 03:01:43 -0800

    Added a method to verify account's by using get method and params instead of post

--- routes/confirm.js
+++ routes/confirm.js
@@ -19,10 +19,75 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //add a get route to the router. 
 router.get("/", (req, res) => {
-    res.send({
-        message: "Hello, you sent a GET request"
-        
-    });
+    
+    // Validate the token
+
+    let token = req.query['token'];
+
+    if (!token) {
+        return res.status(400).send({
+            success: false,
+            msg: "Token cannot be blank"
+        });
+    } else {
+        // Process the token and check if we should validate the user
+        // Check the database to see if there is a token matching the given token.
+        db.one('SELECT MemberID, Token FROM VerificationToken WHERE Token=$1', [token])
+        .then(row => { //If successful, run function passed into .then() 
+
+            let memberID = row['memberid'];
+
+            db.one('SELECT FirstName, Verification FROM Members WHERE MemberID = $1', [memberID, token])
+            .then(row => { //If successful we found a matching token
+            // If we made it this far we know we have a user with a matching token.
+            // Check to see if the user has been verified
+            let isVerified = row['verification'];
+            let firstName = row['firstname'];
+
+            // If the user has been verified return and end these shenanigan's
+            if (isVerified) {
+                return res.status(400).send({
+                    success: false, 
+                    msg: 'This user has already been verified.' });
+            // We need to validate the user
+            } else {
+                db.none("UPDATE Members SET Verification = $1 WHERE MemberID = $2", [1, memberID])
+                .then(() => {
+
+                // We successfully verified the user
+                console.log("check the Members table and see if we have a verified member!");
+                res.status(200).send({
+                    success: true,
+                    msg: "The account has been verified. Please log in."
+                });
+
+                })
+                .catch((err) => {
+                    res.status(418).send({
+                        success: false,
+                        error: "We couldn't set isVerified to true"
+                    });
+                });
+            }
+            })
+            .catch((err) => { // 
+                res.status(400).send({
+                    success: false,
+                    error: "Couldn't find a user with that token"
+                });
+            });
+        })
+        .catch((err) => {
+            //log the error
+            res.status(401).send({
+                success: false,
+                error: 'Invalid token'
+            });
+        });
+
+
+    }
+
 });
 
 // Handle the post with the token
@@ -72,24 +137,21 @@ router.post('/', urlencodedParser, (req, res) => {
                     msg: "The account has been verified. Please log in."
                 });
 
+                })
+                .catch((err) => {
+                    res.status(418).send({
+                        success: false,
+                        error: "We couldn't set isVerified to true"
+                    });
+                });
+            }
             })
-            .catch((err) => {
-                res.status(418).send({
+            .catch((err) => { // 
+                res.status(400).send({
                     success: false,
-                    error: "We couldn't set isVerified to true"
+                    error: "Couldn't find a user with that token"
                 });
             });
-            }
-        })
-        .catch((err) => { // 
-            res.status(400).send({
-                success: false,
-                error: "Couldn't find a user with that token"
-            });
-        });
-
-
-
         })
         .catch((err) => {
             //log the error
