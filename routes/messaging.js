be11686afde26d81b4d6fae1b1b46e8af132e6aa
arshdeepgill <arshgill277@gmail.com>
2019-03-11 03:38:44 -0700

    changed line 36 in messaging.js

--- routes/messaging.js
+++ routes/messaging.js
@@ -33,7 +33,7 @@ router.post("/send", (req, res) => {
     .then(() => {
 
         //send a notification of this message to ALL members with registered tokens
-        db.manyOrNone('SELECT * FROM Push_Token')
+        db.manyOrOne('SELECT token FROM Push_Token WHERE MEMBERID IN (SELECT MEMEBERID FROM CHATMEMBERS WHERE CHATID = $1)',[chatId])
         .then(rows => {
             rows.forEach(element => {
                 msg_functions.sendToIndividual(element['token'], message, email);
